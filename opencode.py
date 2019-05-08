9b00abe9b1aa5c92b7ca7d94a507428a26efcb87
Timothy Liu <tlkh.xms@gmail.com>
2019-05-08 10:00:27 +0800

    Update opencode.py

--- opencode.py
+++ opencode.py
@@ -1,6 +1,6 @@
 def setup_opencode():
     return {
-        "command": ["code-server", "-p", "{port}", "--no-auth", "--allow-http", "-d", "/home/jovyan"],
+        "command": ["code-server", "-p", "{port}", "--no-auth", "--allow-http", "-d", "/home/jovyan", "--disable-telemetry", "--user-data-dir", "/home/jovyan/vscode/"],
         "absolute_url": False,
         "launcher_entry": {
             "title": "VS Code",
