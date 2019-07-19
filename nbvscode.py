d903462b48118426a1b193fbaccfe13fce85451b
Timothy Liu <tlkh.xms@gmail.com>
2019-07-19 13:29:40 +0800

    Adjust launch config

--- nbvscode.py
+++ nbvscode.py
@@ -3,7 +3,7 @@ def setup_nbvscode():
         "command": ["code-server", "-p", "{port}",
                     "--no-auth", "--allow-http", "--disable-telemetry",
                     "-e", "/home/jovyan/vscode_extensions/",
-                    "-d", "/home/jovyan/"],
+                    "-d", "/home/jovyan/vscode/"],
         "absolute_url": False,
         "launcher_entry": {
             "title": "VS Code",
